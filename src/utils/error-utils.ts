5e52114f5c40dc88cb7c563142d85baf1ff71423
IcikAltman <icikaltman@gmail.com>
2026-06-04 10:12:34 +0300

    run lint

--- src/utils/error-utils.ts
+++ src/utils/error-utils.ts
@@ -1,8 +1,9 @@
 export enum ErrorCode {
-    BAD_REQUEST = 400,
-    UNAUTHORIZED = 403,
-    NOT_FOUND = 404,
-    SERVER_ERROR = 500,
+	BAD_REQUEST = 400,
+	UNAUTHORIZED = 403,
+	NOT_FOUND = 404,
+	SERVER_ERROR = 500,
 }
 
-export const isErrorCode = (code: number) => Object.values(ErrorCode).includes(code)
+export const isErrorCode = (code: number) =>
+	Object.values(ErrorCode).includes(code)
