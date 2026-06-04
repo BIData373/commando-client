c8676a4f8554f72763eb3793bdb9dca573d759c1
IcikAltman <icikaltman@gmail.com>
2026-06-04 12:32:29 +0300

    fix the import of EmptyState

--- src/components/Tasks/TasksLayout.tsx
+++ src/components/Tasks/TasksLayout.tsx
@@ -24,7 +24,7 @@ import {
 	DropdownMenuTrigger,
 } from "../ui/dropdown-menu"
 import { TooltipProvider } from "../ui/tooltip"
-import { NoResultsFound } from "./EmptyState"
+import { EmptyState } from "./EmptyState"
 import { TaskCardGrid } from "./TaskCardGrid"
 import { TaskFilters } from "./TaskFilters"
 import { TaskTable } from "./TaskTable"
@@ -222,9 +222,9 @@ function TasksLayout({
 
 				<ContentArea>
 					{tasks.length === 0 ? (
-						<NoResultsFound variant="empty" />
+						<EmptyState />
 					) : searchQuery && filteredTasks.length === 0 ? (
-						<NoResultsFound variant="no-search-results" />
+						<EmptyState variant="search" />
 					) : view === TasksView.TABLE ? (
 						<TaskTable
 							queryKey={tasksQueryKey}
