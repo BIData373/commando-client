7201ff52a008c0a191f874a81b09ea0366f295a4
Nir Finkleshtein <s9336069@idfts.il>
2026-06-08 20:12:15 +0300

    Add matomo wrapper

---
+++ src/wrappers/MatomoWrapper.tsx
@@ -0,0 +1,67 @@
+import { type PropsWithChildren, useEffect } from "react"
+import type { UserInfoDto } from "src/api/model"
+
+declare global {
+	interface Window {
+		_paq: unknown[][]
+	}
+}
+
+export default function MatomoWrapper({ children }: PropsWithChildren) {
+	useEffect(() => {
+		const cookieString = document.cookie
+		const result = cookieString
+			?.match(/ssoUser=([^;]+)/)
+			?.pop()
+			?.replaceAll('"', "")
+		const base64Url = result?.split(".")?.[1]
+		const base64 = base64Url?.replace(/-/g, "+")?.replace(/_/g, "/")
+		const jsonPayload = base64
+			? decodeURIComponent(
+					atob(base64)
+						?.split("")
+						?.map((c) => `%${(`00${c.charCodeAt(0).toString(16)}`).slice(-2)}`)
+						.join(""),
+				)
+			: ""
+
+		let resultParsed: UserInfoDto | undefined
+
+		try {
+			resultParsed = JSON.parse(jsonPayload)?.user
+		} catch {
+			resultParsed = undefined
+		}
+
+		const userName = resultParsed?.displayName
+		const privateNumber = resultParsed?.upn
+
+		window._paq = window._paq || []
+		const _paq = window._paq
+
+		_paq.push(["setDocumentTitle", `${document.domain}/${document.title}`])
+		_paq.push(["setCookieDomain", "*.vector.idf.cts"])
+		_paq.push(["trackPageView"])
+		_paq.push(["enableLinkTracking"])
+		_paq.push([
+			"setUserId",
+			privateNumber && userName ? `${privateNumber}\n${userName}` : "",
+		])
+
+		const u = "//matomo.idf.cts/"
+
+		_paq.push(["setTrackerUrl", `${u}matomo.php`])
+		_paq.push(["setSiteId", "2073"])
+
+		const d = document,
+			g = d.createElement("script"),
+			s = d.getElementsByTagName("script")[0]
+
+		g.type = "text/javascript"
+		g.async = true
+		g.src = `${u}matomo.js`
+		s.parentNode?.insertBefore(g, s)
+	}, [])
+
+	return children
+}
