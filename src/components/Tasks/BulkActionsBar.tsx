abc6f313af149f469bf6d91844e309ae494f4a59
rocketdv <leviu02@gmail.com>
2026-06-10 02:18:01 +0300

    fix: remove archive button

--- src/components/Tasks/BulkActionsBar.tsx
+++ src/components/Tasks/BulkActionsBar.tsx
@@ -49,10 +49,6 @@ export function BulkActionsBar({
 						</GhostButton>
 					}
 				/>
-				<GhostButton onClick={onArchive}>
-					העבר לארכיון
-					<Archive size={16} />
-				</GhostButton>
 				<BarDivider />
 				<DropdownMenu>
 					<DropdownMenuTrigger asChild>
@@ -82,7 +78,7 @@ const Bar = styled.div`
   transform: translateX(-50%);
   z-index: var(--z-dropdown);
   direction: ltr;
-  width: 627px;
+  width: 450px;
   display: flex;
   align-items: center;
   justify-content: space-between;
