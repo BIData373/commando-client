050dc5f99b5490a97e9cc0d3e228a15f513d7c7a
Nir Finkleshtein <s9336069@idfts.il>
2026-06-09 20:25:37 +0300

    INFOSYS-143 Make date picker filter out tasks without dates

--- src/utils/date-utils.ts
+++ src/utils/date-utils.ts
@@ -1,5 +1,4 @@
 import type { DateRange } from "react-day-picker"
-import type { TaskDto } from "src/api/model"
 
 export enum DATE_TYPE {
 	CREATION_DATE = "תאריך יצירה",
@@ -8,21 +7,4 @@ export enum DATE_TYPE {
 	UPDATED_DATE = "תאריך עדכון",
 }
 
-export function getTaskDateByDateType<TTask extends TaskDto>(
-	task: TTask,
-	type: DATE_TYPE,
-): Date | null {
-	switch (type) {
-		case DATE_TYPE.CREATION_DATE:
-			return task.createdAt
-		case DATE_TYPE.EXPECTED_END:
-			return task.dueDate ?? null
-		case DATE_TYPE.ISSUE_DATE:
-			return task.source?.date ?? null
-		case DATE_TYPE.UPDATED_DATE:
-			return task.updatedAt
-		default:
-			return task.createdAt
-	}
-}
 export type DatePickerValue = Date | DateRange
