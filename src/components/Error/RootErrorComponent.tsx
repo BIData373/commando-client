b1dd7493b71b21c2f5e22e65dc07cfb4bb228c50
Nir Finkleshtein <s9336069@idfts.il>
2026-06-10 03:44:18 +0300

    Add useErrorHandler

--- src/components/Error/RootErrorComponent.tsx
+++ src/components/Error/RootErrorComponent.tsx
@@ -1,16 +1,9 @@
-import { useEffect } from "react"
-import { useErrorModal } from "src/providers/ErrorModalProvider"
+import { useErrorHandler } from "src/providers/ErrorModalProvider"
 
 interface RootErrorComponentProps {
 	error: Error
 }
 
 export function RootErrorComponent({ error }: RootErrorComponentProps) {
-	const { handleError } = useErrorModal()
-
-	useEffect(() => {
-		handleError(error)
-	}, [error, handleError])
-
-	return null
+	useErrorHandler(error)
 }
