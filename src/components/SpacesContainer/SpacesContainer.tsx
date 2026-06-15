83f035db674498f5e6ee1397d04c8fe08364be64
Leo Ukraincik <45918762+rocketdv@users.noreply.github.com>
2026-06-15 13:51:43 +0300

    Chore: Small Tweaks to the Home Page (#99)
    * chore: small tweaks to the home page
    
    * chore: initials font-size

--- src/components/SpacesContainer/SpacesContainer.tsx
+++ src/components/SpacesContainer/SpacesContainer.tsx
@@ -29,6 +29,6 @@ const SectionTitle = styled.h2`
 
 const WorkspaceGrid = styled.div`
   display: grid;
-  grid-template-columns: repeat(auto-fill, minmax(260px, 1fr));
+  grid-template-columns: repeat(auto-fill, 160px);
   gap: 16px;
 `
