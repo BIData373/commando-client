88d1246aa213426c846bc4970eab1493e263fd65
rocketdv <leviu02@gmail.com>
2026-06-16 13:28:15 +0300

    chore: clean up
    g

--- src/utils/icon-utils.ts
+++ src/utils/icon-utils.ts
@@ -1,11 +1,7 @@
-import { faker } from "@faker-js/faker"
 import { MESIBA_S3_URL } from "./env-utils"
 
 export function formatMesibaIcon(iconName?: string | null) {
 	try {
-		if (Math.random() > 0.5) {
-			return faker.image.avatarGitHub()
-		}
 		return MESIBA_S3_URL && iconName
 			? new URL(iconName, MESIBA_S3_URL).href
 			: (iconName ?? undefined)
