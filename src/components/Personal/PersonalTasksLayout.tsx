587bd33d418a5ba9bc1839a030fb803f4258c1c2
IcikAltman <icikaltman@gmail.com>
2026-07-02 09:43:23 +0300

    Fix: Count Affect Only By Column Filter

--- src/components/Personal/PersonalTasksLayout.tsx
+++ src/components/Personal/PersonalTasksLayout.tsx
@@ -118,8 +118,8 @@ function PersonalTasksLayout() {
 				/>
 
 				<TaskFilters
-					taskRows={filteredTaskRows}
-					allTasksLength={allTaskRows.length}
+					allTasks={allTaskRows}
+					filteredTaskRows={filteredTaskRows}
 					extraColumnsMeta={[{ id: "workspace", label: "מפקד מנחה" }]}
 					startSlot={<TasksDatePicker />}
 					extraFilters={
