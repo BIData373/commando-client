83c91a716b2db60e82b3fc82352a88c2aa475d85
Nir Finkleshtein <s9336069@idfts.il>
2026-07-13 15:12:02 +0300

    Remove week number from deadline field

--- src/components/CreateTasks/DeadlineField.tsx
+++ src/components/CreateTasks/DeadlineField.tsx
@@ -81,6 +81,7 @@ function DeadlineField({
 								mode={CalendarMode.Single}
 								selected={dueDate ?? undefined}
 								onSelect={handleDateSelect}
+								showWeekNumber={false}
 							/>
 						</DatePopoverContent>
 					</Popover>
