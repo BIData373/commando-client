d82f30aae7da7072fdadfbeb83169e79a79da417
Nir Finkleshtein <s9336069@idfts.il>
2026-07-16 19:58:50 +0300

    INFOSYS-3702 fix hebrew

--- src/utils/date-utils.ts
+++ src/utils/date-utils.ts
@@ -3,7 +3,7 @@ import type { DateRange } from "react-day-picker"
 export enum DATE_TYPE {
 	CREATION_DATE = "תאריך יצירה",
 	EXPECTED_END = 'לפי תג"ב',
-	ISSUE_DATE = "תאריך מתן הנחייה",
+	ISSUE_DATE = "תאריך מתן הנחיה",
 	UPDATED_DATE = "תאריך עדכון",
 }
 
