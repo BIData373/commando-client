5497f10f7f372749553d00b3e239a0dc2456908e
Nir Finkleshtein <s9336069@idfts.il>
2026-08-16 13:49:43 +0300

    Fix max depth exceeded error

--- src/providers/HeaderProvider.tsx
+++ src/providers/HeaderProvider.tsx
@@ -55,5 +55,5 @@ export function useRenderInHeader(
 		setElementPlacements((prev) => ({ ...prev, [key]: node }))
 
 		return () => setElementPlacements(({ [key]: _, ...rest }) => rest)
-	}, [setElementPlacements, ...deps, key, node])
+	}, [setElementPlacements, ...deps, key])
 }
