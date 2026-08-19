0f873a2ddb7cfe2cd3e5cd21eb470461d3a925b0
yovel efraim <yovel.12011@gmail.com>
2026-08-19 13:58:43 +0300

    add comments column to table

--- src/components/TaskDetail/TaskCommentsSection.tsx
+++ src/components/TaskDetail/TaskCommentsSection.tsx
@@ -7,6 +7,7 @@ import {
 	useDeleteMessage,
 	useListMessages,
 } from "src/api/message/message"
+import { getListTaskRowsQueryKey } from "src/api/task/task"
 import { useCurrentUser } from "src/hooks/useCurrentUser"
 import { invalidateQueries } from "src/queryClient"
 import { formatDateMonthYear, formatMinutesHours } from "src/utils/time-format"
@@ -64,7 +65,10 @@ function TaskCommentsSection({
 			{
 				onSuccess() {
 					setCommentValue("")
-					invalidateQueries([getListMessagesQueryKey({ taskId })])
+					invalidateQueries([
+						getListMessagesQueryKey({ taskId }),
+						getListTaskRowsQueryKey(),
+					])
 				},
 			},
 		)
