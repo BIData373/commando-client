84e185e0b755481ef6285a50fea9374c14014b2a
JSoot8 <jsoot8@gmail.com>
2026-09-07 19:20:02 +0300

    Feat: Add first time redirect

--- src/routes/workspace/$urlName/index.tsx
+++ src/routes/workspace/$urlName/index.tsx
@@ -1,16 +1,23 @@
+import { readLocalStorageValue } from "@mantine/hooks"
 import { createFileRoute, redirect } from "@tanstack/react-router"
+
+localStorage.setItem("workspaceFirstTime", "true")
 
 export const Route = createFileRoute("/workspace/$urlName/")({
 	beforeLoad: ({ params }) => {
-		/*if(DB_VAR) {
+		const needsAssigneesRedirect = readLocalStorageValue({
+			key: "workspaceFirstTime",
+		})
+		if (needsAssigneesRedirect) {
+			localStorage.setItem("workspaceFirstTime", "false")
 			throw redirect({
-				to: "/workspace/$urlName/settings/assignees",
+				to: "/workspace/$urlName/settings/assignees/help",
 				params,
 			})
-		}*/
-		throw redirect({
-			to: "/workspace/$urlName/dashboard",
-			params,
-		})
+		} else
+			throw redirect({
+				to: "/workspace/$urlName/dashboard",
+				params,
+			})
 	},
 })
