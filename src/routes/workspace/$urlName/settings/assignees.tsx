f9a3483533e5c535641f4bc4a8cbb2fa970b2968
SuperDoubleDuck <JSoot8@gmail.com>
2026-09-23 13:46:17 +0300

    INFOSYS-3887 Assignees Redirect and Introduction (#186)
    * style: homepage scroll and size
    
    * Feat: Assignee info modal and redirect on first open
    
    * Feat: Add first time redirect
    
    * bugfix: fix viewer being directed to unaccessable page
    
    * CR Fixes
    
    * CR Fixes
    
    * Conflict Fix
    
    * CR Fixes

--- src/routes/workspace/$urlName/settings/assignees.tsx
+++ src/routes/workspace/$urlName/settings/assignees.tsx
@@ -1,7 +1,11 @@
 import styled from "@emotion/styled"
-import { createFileRoute, Outlet } from "@tanstack/react-router"
+import { createFileRoute, Outlet, useNavigate } from "@tanstack/react-router"
+import { useListAssignees } from "src/api/assignee/assignee"
 import { AssigneesContent } from "src/components/settings/AssigneesContent"
+import { AssigneesEmptyState } from "src/components/settings/AssigneesEmptyState"
+import { CircleHelpButton } from "src/components/settings/CircleHelpButton"
 import { SectionTitle } from "src/components/settings/SectionTitle"
+import { useWorkspace } from "src/providers/WorkspaceProvider"
 import { SETTINGS_TABS, SettingTabPath } from "src/utils/settings-utils"
 
 export const Route = createFileRoute("/workspace/$urlName/settings/assignees")({
@@ -11,12 +15,47 @@ export const Route = createFileRoute("/workspace/$urlName/settings/assignees")({
 const activeTab = SETTINGS_TABS[SettingTabPath.ASSIGNEES]
 
 function SettingsAssignees() {
+	const {
+		workspace: { id: workspaceId, urlName },
+	} = useWorkspace()
+	const { data: assignees = [] } = useListAssignees({ workspaceId })
+
+	const isEmptyState = assignees.length === 0
+
+	const navigate = useNavigate()
+
+	function handleInfoModalOpen() {
+		navigate({
+			to: "/workspace/$urlName/settings/assignees/help",
+			params: { urlName },
+		})
+	}
+
+	function handleOpenCreateDialog() {
+		navigate({
+			to: "/workspace/$urlName/settings/assignees/new",
+			params: { urlName },
+		})
+	}
+
 	return (
 		<AssigneesRoot>
-			<SectionTitle title={`ניהול אחראים - ${activeTab}`} />
+			{!isEmptyState && (
+				<TitleContainer>
+					<SectionTitle title={`ניהול אחראים - ${activeTab}`} />
+					<CircleHelpButton onClick={handleInfoModalOpen} />
+				</TitleContainer>
+			)}
 
 			<CardScrollArea>
-				<AssigneesContent />
+				{isEmptyState ? (
+					<AssigneesEmptyState
+						onOpenCreateDialog={handleOpenCreateDialog}
+						onOpenInfoModal={handleInfoModalOpen}
+					/>
+				) : (
+					<AssigneesContent onOpenCreateDialog={handleOpenCreateDialog} />
+				)}
 			</CardScrollArea>
 
 			<Outlet />
@@ -39,4 +78,8 @@ const CardScrollArea = styled.div`
   overflow: hidden;
   display: flex;
   flex-direction: column;
+`
+const TitleContainer = styled.div`
+  display: flex;
+  align-items: center;
 `
